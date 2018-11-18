dcc286cc203cfb93406fab9bb64d92433abcd5f4
Tyler <tyler.mcrobert@gmail.com>
2018-11-18 14:00:59 -0800

    Viewer style updates

--- src/containers/Viewer/Viewer.js
+++ src/containers/Viewer/Viewer.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { RichText } from 'prismic-reactjs';
 import './Viewer.scss';
 
@@ -12,7 +13,7 @@ export default class Viewer extends React.Component {
     } = this.props;
     const titleVal = RichText.asText(title);
     const imageList = images.map(img => (
-      <img src={img.url} alt={`${titleVal} - Cassidy Villanos`} />
+      <img src={img.url} key={img.url} alt={`${titleVal} - Cassidy Villanos`} />
     ));
 
     return (
@@ -21,16 +22,17 @@ export default class Viewer extends React.Component {
           <div className="viewer__header__title">
             <h1>⟵</h1>
             <h1>
-              0{index + 1}. {RichText.asText(title)}
+              {`0${index + 1}. ${RichText.asText(title)}`}
+              <div className="viewer__close" onClick={() => selectProject(null)}>
+                ✕
+              </div>
             </h1>
             <h1>⟶</h1>
           </div>
           <div className="viewer__desc">
             {RichText.render(description)}
           </div>
-          <div className="viewer__close" onClick={() => selectProject(null)}>
-            [ close ]
-          </div>
+
         </div>
         <div className="viewer__image">
           {imageList}
@@ -41,4 +43,6 @@ export default class Viewer extends React.Component {
 }
 
 Viewer.propTypes = {
+  selectProject: PropTypes.func.isRequired,
+  index: PropTypes.number.isRequired,
 };
