f1e46ec306fb8431f69f3c139a3d15de64761226
Tyler <tyler.mcrobert@gmail.com>
2018-12-24 06:37:06 -0600

    Add preview

--- src/containers/Router/Router.js
+++ src/containers/Router/Router.js
@@ -8,6 +8,7 @@ const Router = () => (
       <Route exact path="/work/:uid" render={() => <App view="viewer" />} />
       <Route exact path="/" render={() => <App view="home" />} />
       <Route exact path="/about" render={() => <App view="about" />} />
+      <Route exact path="/preview" render={() => <App view="home" />} />
       <Route path="/" render={() => <div className="notFound">404—Not Found</div>} />
     </Switch>
   </BrowserRouter>
