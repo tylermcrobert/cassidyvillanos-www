af77c25a0ede2d2fe7ecdce4f3ea28e95ea3de19
Tyler <tyler.mcrobert@gmail.com>
2019-02-14 19:01:58 -0800

    Plugins and bullshit

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Microsite Playground',
+    description: 'Messing around with Gatsby',
+  },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-prismic',
+      options: {
+        repositoryName: 'cassidyvillanos',
+        linkResolver: ({ node, key, value }) => post => `/${post.uid}`,
+      },
+    },
+  ],
+};
