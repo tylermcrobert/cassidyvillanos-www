15baeae4ce4df29954a73838d45bac0a740fd012
Tyler <tyler.mcrobert@gmail.com>
2019-02-14 20:09:38 -0800

    Pulled in data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,54 @@
-import React from "react";
-import {graphql, useStaticQuery} from 'gatsby';
+import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 
-export default () => <div>Hello world!</div>
+export default () => {
+  const projectsQuery = useStaticQuery(graphql`
+    {
+      prismicSite {
+        data {
+          site_name
+            projects{
+              project{
+                document {
+                  uid
+                  data{
+                    title{
+                      text
+                    }
+                    images{
+                      image{
+                        url
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+  `).prismicSite.data.projects.map((item) => {
+    const projData = item.project.document[0];
+    return {
+      uid: projData.uid,
+      image: projData.data.images[0].image.url,
+      title: projData.data.title.text,
+    };
+  });
+
+  console.log(projectsQuery);
+
+  return (
+    <Index projects={projectsQuery} />
+  );
+};
+
+
+function Index({ projects }) {
+  return projects.map(({ uid, image, title }) => (
+    <a key={uid} href={`#${uid}`}>
+      <img src={image} alt="" width={200} />
+      {title}
+    </a>
+  ));
+}
