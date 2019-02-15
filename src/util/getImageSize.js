e1603551929152839159e5e12ef429104d71067a
Tyler <tyler.mcrobert@gmail.com>
2019-02-14 18:44:16 -0800

    Hello World

--- src/util/getImageSize.js
+++
@@ -1,8 +0,0 @@
-const getImageSize = (img, size) => {
-  if (img) {
-    return (img[size]) ? img[size].url : img.url;
-  }
-  return null;
-};
-
-export default getImageSize;
--- src/util/leadingZero.js
+++
@@ -1,3 +0,0 @@
-const leadingZero = num => num.toString().padStart(2, 0);
-
-export default leadingZero;
